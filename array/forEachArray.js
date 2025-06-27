3fb8f2816dfe40eb8b868cc141726942f704ca72
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-06-27 23:31:50 +0530

    feat: forEachArray

---
+++ array/forEachArray.js
@@ -0,0 +1,9 @@
+// 1. with method
+const fruits = ["apple", "banana", "cherry"];
+fruits.forEach((fruit) => console.log(fruit));
+
+// 2. with for loop
+const fruits2 = ["apple", "banana", "cherry"];
+for (let i = 0; i < fruits2.length; i++) {
+    console.log(fruits2[i]);
+}
